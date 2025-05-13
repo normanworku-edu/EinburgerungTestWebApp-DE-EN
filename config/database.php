e95a94b1acafbbf41057fdca14c07ddd0d10aaaa
Norman Worku <norman.worku@tooztech.com>
2025-05-13 17:34:16 +0200

    working

--- config/database.php
+++ config/database.php
@@ -1,15 +1,16 @@
 <?php
 class Database {
     private $host = 'localhost';
-    private $dbname = 'exam_platform';
+    private $port = 3306;
+    private $dbname = 'german_citizenship_test_db';
     private $username = 'root';
-    private $password = '';
+    private $password = 'root';
     private $conn;
 
     public function connect() {
         try {
             $this->conn = new PDO(
-                "mysql:host=$this->host;dbname=$this->dbname;charset=utf8mb4",
+                "mysql:host=$this->host;port=$this->port;dbname=$this->dbname;charset=utf8mb4",
                 $this->username,
                 $this->password,
                 [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
---
+++ indexxx.php
@@ -0,0 +1,4 @@
+<html>
+Hello World
+<?php phpinfo(); ?>
+</html>
